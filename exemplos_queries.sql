fc7ceaff8a1e96c82d6f16437e9b69fdf048419a
Marcos Aurelio Silverio <marcos.aurelio@infracerta.com.br>
2019-12-21 18:31:26 +0000

    Adicionado exemploes_queries.sql

---
+++ exemplos_queries.sql
@@ -0,0 +1,37 @@
+USE otrs;
+
+SHOW TABLES;
+
+SELECT * FROM ticket;
+
+SELECT id, tn, title, customer_user_id, create_time, ticket_state_id FROM ticket;
+
+SELECT 
+ticket.id, 
+ticket.tn, 
+ticket.title,
+ticket.customer_user_id, 
+ticket.create_time,
+ticket_state.name
+FROM ticket INNER JOIN ticket_state ON ticket.ticket_state_id = ticket_state.id;
+
+SELECT 
+ticket.id AS ID, 
+ticket.tn AS NUMERO, 
+ticket.title AS ASSUNTO,
+ticket.customer_user_id AS CLIENTE, 
+ticket.create_time AS CRIADO,
+ticket_state.name AS ESTADO
+FROM ticket INNER JOIN ticket_state ON ticket.ticket_state_id = ticket_state.id;
+
+SELECT 
+ticket.id AS ID, 
+ticket.tn AS NUMERO, 
+ticket.title AS ASSUNTO,
+ticket.customer_user_id AS CLIENTE, 
+ticket.create_time AS CRIADO,
+ticket_state.name AS ESTADO
+FROM ticket INNER JOIN ticket_state ON ticket.ticket_state_id = ticket_state.id
+WHERE ticket.create_time >= now() - INTERVAL 1 DAY;
+
+SELECT COUNT(*) FROM ticket WHERE create_time >= now() - INTERVAL 1 DAY;
